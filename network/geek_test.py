29af3fbdf5205eb1e38f296f1a48e98da6148124
swapnilAngarkhe <swapnilang21@gmail.com>
2026-09-13 17:38:05 +0530

    github CI testing

--- network/geek_test.py
+++ network/geek_test.py
@@ -1,8 +1,10 @@
 import pytest
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 from .models import Post
 
+User=get_user_model()
 
 @pytest.mark.django_db
 def test_post_creation():
